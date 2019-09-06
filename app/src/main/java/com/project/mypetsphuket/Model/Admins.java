f5c8aa6795f89cd84dd088c89987d2aa5aad910c
bubblebao <mongkonrat.nsw@gmail.com>
2019-09-07 02:29:37 +0700

    New class Admins.class

--- app/src/main/java/com/project/mypetsphuket/Model/Admins.java
+++ app/src/main/java/com/project/mypetsphuket/Model/Admins.java
@@ -4,4 +4,43 @@ public class Admins {
 
     private String name, phone, password, image, address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
