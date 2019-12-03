67b23842f311f46e2347bc592363bcf9a235dc38
bubblebao <mongkonrat.nsw@gmail.com>
2019-12-03 09:50:02 +0700

    Make Map Activity

--- app/src/main/java/com/project/mypetsphuket/MapActivity.java
+++ app/src/main/java/com/project/mypetsphuket/MapActivity.java
@@ -35,11 +35,9 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
             // Add a marker in Sydney, Australia, and move the camera.
             LatLng sydney = new LatLng(7.888367 , 98.381493);
-
-            // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").draggable(true).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_doctor_load)));
-            //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_doc)));
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney , 18) ,2500 , null);    //Scope x 18 , Speed much Low
-
+            //   mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromFile("https://firebasestorage.googleapis.com/v0/b/my-pets-phuket-bb27e.appspot.com/o/Doctors%2FDr.%20Ashish%20Simson.jpg?alt=media&token=2aaf2d51-2bae-483d-a73d-57f0f78284f6")));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney , 18) , 5000 , null);    //Scope x 18 , Speed much Low
+          //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
