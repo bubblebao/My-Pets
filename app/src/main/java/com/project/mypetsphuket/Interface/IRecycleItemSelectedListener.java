8d4e01ac8ec55cc9f15a3d646103807549228e95
bubblebao <mongkonrat.nsw@gmail.com>
2020-01-27 21:59:04 +0700

    Book Fragment1 Service Fixbug

---
+++ app/src/main/java/com/project/mypetsphuket/Interface/IRecycleItemSelectedListener.java
@@ -0,0 +1,8 @@
+package com.project.mypetsphuket.Interface;
+
+import android.view.View;
+
+public interface IRecycleItemSelectedListener {
+
+    void onItemSelectedListener(View view,int position);
+}
